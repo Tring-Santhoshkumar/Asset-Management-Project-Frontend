e0e861048c1d0148f5c086b37517562cdf3f3a61
Santhosh Kumar A <santhoshkumar.a@tringapps.com>
2025-03-09 17:25:21 +0530

    Update ApolloClient.tsx

--- src/ApolloClient.tsx
+++ src/ApolloClient.tsx
@@ -3,9 +3,6 @@ import { ApolloClient , InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
     uri : "http://localhost:3001/",
     cache : new InMemoryCache(),
-    headers: {
-        "Content-Type": "application/json",
-    },
 })
 
 export default client;
